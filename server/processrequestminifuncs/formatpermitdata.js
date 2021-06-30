339462f8b0a4dcd22bf6496f5da19dcb3a774e6e
Nissim <nissimram1812@gmail.com>
2021-06-30 17:22:11 -0400

    formatPermitData function is complete v0.1

--- server/processrequestminifuncs/formatpermitdata.js
+++ server/processrequestminifuncs/formatpermitdata.js
@@ -2,15 +2,23 @@ function formatPermitData(rawData, borough) {
     //format each object into an address, owner info, and coordinates section 
     const formattedData = rawData.map(building => {
 
-        const address = `${building.house__} ${building.street_name}, ${borough} ${building.zip_code}`
+        const address = `${building.house__} ${building.street_name}, ${borough}, ${building.zip_code}`
 
-        const ownerinfo = `${building.owner_s_first_name} ${building.owner_s_last_name} 
-            Owner's Business Name: ${building.owner_s_business_name}
-            Permittee's Business Name : ${building.permittee_s_business_name}`
+        const ownerBizName = `${building.owner_s_first_name} ${building.owner_s_last_name}`
 
-        const coordinates = [building.gis_latitude, building.gis_longitude]
+        const ownerName = `Owner's Business Name: ${building.owner_s_business_name}`
 
-        return { address: address, ownerinfo: ownerinfo, coordinates: coordinates }
+        const permitInfo = `Permittee's Business Name : ${building.permittee_s_business_name}`
+
+        const coordinates = [parseFloat(building.gis_latitude), parseFloat(building.gis_longitude)]
+
+        return { //each element of the new array will be made up of these objects
+            address: address,
+            ownerBizName: ownerBizName,
+            ownerName: ownerName,
+            permitInfo: permitInfo,
+            coordinates: coordinates
+        }
     })
 
     console.log(formattedData);
