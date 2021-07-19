31da66c576e1fb23c5bc04ec4a355c9d4ccd38d7
Nissim <nissimram1812@gmail.com>
2021-07-18 21:42:22 -0700

    styling line graph

--- client/nyc-open-data/src/components/charts/linechart.js
+++ client/nyc-open-data/src/components/charts/linechart.js
@@ -6,52 +6,94 @@ const LineChart = (props) => {
 
     console.log(props.dataPoints)
 
-    const [NBcounts, DMcounts, A1counts] = props.dataPoints;
+    const [NBobj, DMobj, A1obj] = props.dataPoints;
 
-    console.log(NBcounts)
+
 
     const yearLabels = props.dataPoints[0].map(el => {
         return el.year
     })
 
-    console.log(yearLabels)
+    //console.log(yearLabels)
 
+    const NBcount = NBobj.map(el => {
+        return el.count
+    })
 
+    const DMcount = DMobj.map(el => {
+        return el.count
+    })
 
+    const A1count = A1obj.map(el => {
+        return el.count
+    })
+
+    console.log(DMcount);
+
+    console.log(NBcount);
     return (
-        <div className="lineGraph_wrapper">
+        <div className="outter_linegraph_wrapper">
 
-            <Line
-                height={10}
-                width={10}
-                data={{
-                    //labels: ["Demolitions", "New Buildings", "Building Alterations"],
-                    labels: yearLabels,
-                    datasets: [{
-                        //data: props.dataPoints,
-                        data: [1, 2, 4, 5, 6],
-                        backgroundColor: [
-                            "#D93710", "#2F78CE", "#EB9B3B"
-                        ]
-                    }],
-                    labels: ["Demolitions", "New Buildings", "Building Alterations"],
-                }}
-                options={{
-                    maintainAspectRation: false,
-                    responsive: true,
-                    plugins: {
-                        legend: {
-                            labels: {
-                                data: ["Demolitions", "New Buildings", "Building Alterations"],
-                                color: "white",
-                                font: {
-                                    size: "20px"
+            <div className="lineGraph_wrapper">
+
+                <Line
+                    height={5}
+                    width={5}
+                    data={{
+                        //labels: ["Demolitions", "New Buildings", "Building Alterations"],
+                        labels: yearLabels,
+                        //labels: [1990, 1991, 1992, 1993],
+                        datasets: [
+                            {
+                                label: "New Buildings",
+
+                                data: NBcount,
+                                //data: [1, 2, 4, 5, 6],
+                                backgroundColor: [
+                                    "blue" // "#2F78CE", "#EB9B3B"
+                                ],
+                                borderColor: 'rgb(75, 192, 192)',
+                                tension: 0.2
+                            },
+                            {
+                                label: "Demolitions",
+                                data: DMcount,
+                                //data: [1, 2, 4, 5, 6],
+                                backgroundColor: [
+                                    "red" // "#2F78CE", "#EB9B3B"
+                                ],
+                                borderColor: "#BC6176",
+                                tension: 0.2
+                            },
+                            {
+                                label: "Building Alterations",
+                                data: A1count,
+                                backgroundColor: [
+                                    "orange"
+                                ],
+                                tension: 0.2
+                            }
+
+                        ],
+                        //labels: ["Demolitions", "New Buildings", "Building Alterations"],
+                    }}
+                    options={{
+                        maintainAspectRation: false,
+                        responsive: true,
+                        plugins: {
+                            legend: {
+                                labels: {
+                                    data: ["Demolitions", "New Buildings", "Building Alterations"],
+                                    color: "black",
+                                    font: {
+                                        size: "20px"
+                                    }
                                 }
                             }
                         }
-                    }
-                }}
-            />
+                    }}
+                />
+            </div>
         </div>
 
     )
