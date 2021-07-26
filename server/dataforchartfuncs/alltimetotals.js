6f9a6915701f4adb0792861547df50656bfe51c3
Nissim <nissimram1812@gmail.com>
2021-07-26 15:55:36 -0400

    queries are throttled so that data is successfully queried without too many callsin close succession

--- server/dataforchartfuncs/alltimetotals.js
+++ server/dataforchartfuncs/alltimetotals.js
@@ -1,12 +1,15 @@
 //THIS AGGREGATES DATA FOR LINE GRAPH 
 async function getAllTimeTotals() {
+
     require('dotenv').config()
+
+
+    const throttle = require('promise-ratelimit')(25); /* rateInMilliseconds */
+
 
     const fetch = require("node-fetch");
 
     const apiKey = process.env.API_KEY_ID;
-
-    //let year = 1990; // starting year 
 
     const today = new Date();
     const currentYear = today.getFullYear();
@@ -33,9 +36,13 @@ async function getAllTimeTotals() {
         //node rate limiter goes here potentially 
         //map is not ever an async function so you have to wrap it in Promise.all 
         const countObj = await Promise.all(years.map(async year => {
-            const yearWithTotal = await getTotal(jobType, year)
-            //console.log(yearWithTotal[0])
-            return yearWithTotal[0]
+            const result = throttle().then(async function () {
+                const yearWithTotal = await getTotal(jobType, year)
+                //console.log(yearWithTotal)
+                return yearWithTotal[0]
+            })
+            return result // returns a total job count for each year in an object
+
 
         }))
 
@@ -49,33 +56,35 @@ async function getAllTimeTotals() {
                 el.year = years[index]
                 countObjWithYears.push(el)
             } else {
-                //console.log(el)
+                console.log(el)
+                console.log("undefined detected")
             }
         })
-        console.log(countObjWithYears.length)
+        //console.log(countObjWithYears.length)
         return countObjWithYears
 
         //console.log(countObjWithYears);
 
         async function getTotal(jobType, year) {
             //console.log(year)
-            const response = await fetch("https://data.cityofnewyork.us/resource/ipu4-2q9a.json?" +
-                `job_type=${jobType}&` +
+            const response =
 
-                `$where=issuance_date between '${year}-01-01T12:00:00' and '${year + 1}-01-01T12:00:00'&` +
+                await fetch("https://data.cityofnewyork.us/resource/ipu4-2q9a.json?" +
+                    `job_type=${jobType}&` +
 
-                //`$where=issuance_date between '1990-01-01T12:00:00' and '}-01-01T12:00:00'&` +
+                    `$where=issuance_date between '${year}-01-01T12:00:00' and '${year + 1}-01-01T12:00:00'&` +
 
-                `$select=count(job_type)`
+                    //`$where=issuance_date between '1990-01-01T12:00:00' and '}-01-01T12:00:00'&` +
 
-                , { //TIME 
-                    method: "GET",
-                    data: {
-                        "app_token": apiKey
-                    }
-                }).then(response => response.json())
-                //.then(data => console.log(data))
-                .catch(err => console.log(err))
+                    `$select=count(job_type)`
+                    , { //TIME 
+                        method: "GET",
+                        data: {
+                            "app_token": apiKey
+                        }
+                    }).then(response => response.json())
+                    //.then(data => console.log(data))
+                    .catch(err => console.log(err))
 
 
             return response;
@@ -89,11 +98,6 @@ async function getAllTimeTotals() {
     return [newBuildingCount, demolitionCount, buildingAlterationCount]
 }
 
-getAllTimeTotals();
+//getAllTimeTotals();
 
 module.exports = getAllTimeTotals;
-
-
-// message: 'Too many requests',
-// errorCode: 'too-many-requests',
-// data: {}
