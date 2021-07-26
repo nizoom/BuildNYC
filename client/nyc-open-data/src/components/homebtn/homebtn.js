dd9ad39da69b63cd6436615b8a0dcd59483e8131
Nissim <nissimram1812@gmail.com>
2021-07-26 13:43:23 -0400

    added sweet alert message if user forgot to select borough and/or construction type

--- client/nyc-open-data/src/components/homebtn/homebtn.js
+++ client/nyc-open-data/src/components/homebtn/homebtn.js
@@ -1,14 +1,15 @@
 import React from "react";
 import homeImg from "../../media/home.png"
+import "./homebtn.css"
 
 const HomeBtn = () => {
     return (
         <div>
             <a href="https://www.nissimram.com/#/" target="_blank" rel="noopener noreferrer">
-                <button type="button"
+                <button type="button" className="homebtn"
                     style={{
                         borderRadius: "16px", backgroundColor: "green", border: "none", float: "right",
-                        margin: "5px", backgroundImage: "linear-gradient(to right,#9A76CD, #76B5CD)",
+                        margin: "10px", backgroundImage: "linear-gradient(to right,#9A76CD, #76B5CD)",
                         boxShadow: "gray 5px", cursor: "pointer"
                     }}>
 
--- client/nyc-open-data/src/components/submit/submit.js
+++ client/nyc-open-data/src/components/submit/submit.js
@@ -1,4 +1,6 @@
-import React, { useEffect } from "react";
+import React from "react";
+import swal from 'sweetalert';
+
 import "./submit.css"
 
 
@@ -6,12 +8,18 @@ const SubmitBtn = (props) => {
 
     const propsFromUser = props.allUserInputs;
 
-    function assessRequest(propsFromUser) {
+    //console.log(propsFromUser);
+
+    function assessRequest() {
+        const propsFromUser = props.allUserInputs;
+        console.log(propsFromUser.borough); //undefined always / not passed in properly
+        console.log(propsFromUser.job_type); //underfined always
         if (propsFromUser.borough !== "" && propsFromUser.job_type !== "") {
             props.startRequest()
 
         } else {
             console.log("a field is empty")
+            swal("Oops!", "Be sure to select which borough and construction type", "error")
         }; // This is be executed when `loading` state changes
     }
 
@@ -21,7 +29,7 @@ const SubmitBtn = (props) => {
         <div className="submit_btn_wrapper">
             <button type="submit" className="submit_btn" onClick={assessRequest}
             > SUBMIT </button>
-        </div>
+        </div >
     )
 }
 
