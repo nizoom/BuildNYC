22d20da4aea95e9a2d4ee2d79da52ad6b821c957
Nissim <nissimram1812@gmail.com>
2021-07-27 15:06:21 -0400

    added home btn onclick animation

--- client/nyc-open-data/src/components/homebtn/homebtn.js
+++ client/nyc-open-data/src/components/homebtn/homebtn.js
@@ -7,11 +7,7 @@ const HomeBtn = () => {
         <div>
             <a href="https://www.nissimram.com/#/" target="_blank" rel="noopener noreferrer">
                 <button type="button" className="homebtn"
-                    style={{
-                        borderRadius: "16px", backgroundColor: "green", border: "none", float: "right",
-                        margin: "10px", backgroundImage: "linear-gradient(to right,#9A76CD, #76B5CD)",
-                        boxShadow: "gray 5px", cursor: "pointer"
-                    }}>
+                >
 
                     <img src={homeImg} alt="Back to Home" />
 
