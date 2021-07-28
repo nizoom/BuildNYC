e6872f9803867f647f7b806870849e17e0dde10f
Nissim <nissimram1812@gmail.com>
2021-07-28 13:45:27 -0400

    added responsive css to into/header

--- client/nyc-open-data/src/components/intro/intro.js
+++ client/nyc-open-data/src/components/intro/intro.js
@@ -1,18 +1,39 @@
 import React from "react";
 import Typography from '@material-ui/core/Typography'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import "./intro.css"
 
 const Intro = () => {
+
+    const theme = createMuiTheme();
+
+    theme.typography.h1 = {
+        "@media(max-width: 600px)": {
+            fontSize: "4em"
+        },
+        "@media(max-width: 400px)": {
+            fontSize: "3em"
+        }
+    }
+
+    theme.typography.h3 = {
+        "@media(max-width: 600px)": {
+            fontSize: "1.5em"
+        }
+    }
+
     return (
         <div className="outter_wrapper">
-            <div className="title_div">
-                <Typography variant="h1" style={{ color: "white", fontWeight: "300" }}>
-                    <header> Urban Boom </header>
-                </Typography>
-                <Typography variant="h3" style={{ color: "white", fontWeight: "300" }}>
-                    <header> Tracking building development in NYC, 1990 - present </header>
-                </Typography>
-            </div>
+            <ThemeProvider theme={theme}>
+                <div className="title_div">
+                    <Typography variant="h1" style={{ color: "white", fontWeight: "300" }}>
+                        <header> Urban Boom </header>
+                    </Typography>
+                    <Typography variant="h3" style={{ color: "white", fontWeight: "300" }}>
+                        <header> Tracking building development in NYC, 1990 - present </header>
+                    </Typography>
+                </div>
+            </ThemeProvider>
         </div>
     )
 }
